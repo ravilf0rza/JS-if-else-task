c848fac7634f36d63c4992ab38f1484d0e1515cb
ravilf0rza <forzanarchyy@gmail.com>
2024-05-23 22:28:47 +0400

    added new files

---
+++ main.js
@@ -0,0 +1,56 @@
+// let hefteninGunu = prompt("Həftənin gününü seçin");
+// if (hefteninGunu == 1) {
+//     alert ("Bazar ertəsi")
+// }
+// else if(hefteninGunu == 2) {
+//     alert ("Çərşənbə axşamı")
+// }
+// else if (hefteninGunu == 3) {
+//     alert ("Çərşənbə")
+// }
+// else if (hefteninGunu == 4) {
+//     alert ("Cümə axşamı")
+// }
+// else if (hefteninGunu == 5) {
+//     alert ("Cümə")
+// }
+// else if (hefteninGunu == 6) {
+//     alert ("Şənbə")
+// }
+// else if (hefteninGunu == 7) {
+//     alert ("Bazar günü")
+// }
+
+// let havaDurumu = prompt("Sabah havanın yağmurlu olub olmamasını bilmək üçün 1 və ya 2 daxil edin. 1 yağmurlu, 2 isə yağmursuz deməkdir");
+// if (havaDurumu ==1) {
+//     alert ("Yagmurlu")
+    
+// }
+// else if (havaDurumu ==2) {
+//     alert ("Yagmursuz")
+// }
+
+// let age = prompt ("Təqaüd hesablamaları üçün zəhmət olmasa yaşınızı qeyd edin.");
+// if (age > 50) {
+//     alert ("Sizə tam təqaüd hesablanacaq")
+// }
+// else if (age == 50) {
+//     alert ("Sizə yarım təqaüd hesablanacaq")
+// }
+// else {
+//     alert("Sizə təqaüd hesablanmayacaq")
+// }
+
+// let qiymet = prompt ("İmtahanda yığdığınız balları qeyd edin. ");
+// if (qiymet > 90) {
+//     alert ("Imtahandan ugurla kecmisiniz")
+// }
+// else if (qiymet >= 81 && qiymet <= 90) {
+//     alert ("Novbeti merheleye buraxildiniz")
+// }
+// else if (qiymet >=71 && qiymet <=80) {
+//     alert ("Yeniden muraciet edin")
+// }
+// else if (qiymet >=61 && qiymet <=70) {
+//     alert ("Imtahandan kesildiniz")
+// }
